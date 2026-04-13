6e8e1a4f8afba9d00efd23cdb87c614f9c9dcf76
Shubham singh <shubhamsingh@Shubhams-MacBook-Air.local>
2026-04-14 01:44:55 +0530

    added all semester 2 files

--- semester-2/batch-2/lec-7/script.js
+++
@@ -1,9 +0,0 @@
-const input = document.getElementById("locationInput");
-const searchBtn = document.getElementById("search");
-const locationText = document.querySelector(".location");
-
-searchBtn.addEventListener("click", () => {
-  const value = input.value;
-  locationText.textContent = value;
-  input.value = "";
-});
